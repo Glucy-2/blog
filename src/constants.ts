50b2826a52dd813003b4f45bd67b9a4db4c11084
Glucy2 <glucy-2@outlook.com>
2025-09-07 23:00:42 +0800

    update socials and share links

--- src/constants.ts
+++ src/constants.ts
@@ -2,12 +2,12 @@ import type { GiscusProps } from "@giscus/react";
 import type { Props } from "astro";
 import IconMail from "@/assets/icons/IconMail.svg";
 import IconGitHub from "@/assets/icons/IconGitHub.svg";
-import IconBrandX from "@/assets/icons/IconBrandX.svg";
-import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
-import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
-import IconFacebook from "@/assets/icons/IconFacebook.svg";
+// import IconBrandX from "@/assets/icons/IconBrandX.svg";
+// import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
+// import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
+// import IconFacebook from "@/assets/icons/IconFacebook.svg";
 import IconTelegram from "@/assets/icons/IconTelegram.svg";
-import IconPinterest from "@/assets/icons/IconPinterest.svg";
+// import IconPinterest from "@/assets/icons/IconPinterest.svg";
 import { SITE } from "@/config";
 
 interface Social {
@@ -20,65 +20,65 @@ interface Social {
 export const SOCIALS: Social[] = [
   {
     name: "GitHub",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on GitHub`,
+    href: "https://github.com/Glucy-2/blog",
+    linkTitle: `GitHub 上的 ${SITE.title}`,
     icon: IconGitHub,
   },
+  // {
+  //   name: "X",
+  //   href: "https://x.com/username",
+  //   linkTitle: `${SITE.title} on X`,
+  //   icon: IconBrandX,
+  // },
+  // {
+  //   name: "LinkedIn",
+  //   href: "https://www.linkedin.com/in/username/",
+  //   linkTitle: `${SITE.title} on LinkedIn`,
+  //   icon: IconLinkedin,
+  // },
   {
-    name: "X",
-    href: "https://x.com/username",
-    linkTitle: `${SITE.title} on X`,
-    icon: IconBrandX,
-  },
-  {
-    name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
-    linkTitle: `${SITE.title} on LinkedIn`,
-    icon: IconLinkedin,
-  },
-  {
-    name: "Mail",
-    href: "mailto:yourmail@gmail.com",
-    linkTitle: `Send an email to ${SITE.title}`,
+    name: "邮件",
+    href: "mailto:i@glucy2.com",
+    linkTitle: `给 ${SITE.author} 发邮件`,
     icon: IconMail,
   },
 ] as const;
 
 export const SHARE_LINKS: Social[] = [
-  {
-    name: "WhatsApp",
-    href: "https://wa.me/?text=",
-    linkTitle: `Share this post via WhatsApp`,
-    icon: IconWhatsapp,
-  },
-  {
-    name: "Facebook",
-    href: "https://www.facebook.com/sharer.php?u=",
-    linkTitle: `Share this post on Facebook`,
-    icon: IconFacebook,
-  },
-  {
-    name: "X",
-    href: "https://x.com/intent/post?url=",
-    linkTitle: `Share this post on X`,
-    icon: IconBrandX,
-  },
+  // {
+  //   name: "WhatsApp",
+  //   href: "https://wa.me/?text=",
+  //   linkTitle: `Share this post via WhatsApp`,
+  //   icon: IconWhatsapp,
+  // },
+  // {
+  //   name: "Facebook",
+  //   href: "https://www.facebook.com/sharer.php?u=",
+  //   linkTitle: `Share this post on Facebook`,
+  //   icon: IconFacebook,
+  // },
+  // {
+  //   name: "X",
+  //   href: "https://x.com/intent/post?url=",
+  //   linkTitle: `Share this post on X`,
+  //   icon: IconBrandX,
+  // },
   {
     name: "Telegram",
     href: "https://t.me/share/url?url=",
-    linkTitle: `Share this post via Telegram`,
+    linkTitle: `在 Telegram 上分享这篇文章`,
     icon: IconTelegram,
   },
+  // {
+  //   name: "Pinterest",
+  //   href: "https://pinterest.com/pin/create/button/?url=",
+  //   linkTitle: `Share this post on Pinterest`,
+  //   icon: IconPinterest,
+  // },
   {
-    name: "Pinterest",
-    href: "https://pinterest.com/pin/create/button/?url=",
-    linkTitle: `Share this post on Pinterest`,
-    icon: IconPinterest,
-  },
-  {
-    name: "Mail",
-    href: "mailto:?subject=See%20this%20post&body=",
-    linkTitle: `Share this post via email`,
+    name: "邮件",
+    href: "mailto:?subject=看看这篇博客文章: &body=",
+    linkTitle: `通过邮件分享这篇文章`,
     icon: IconMail,
   },
 ] as const;
